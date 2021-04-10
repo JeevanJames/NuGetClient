b8e4c7c8465de49e0f46edb90ec6f8d313189fd2
Jeevan James <jeevanjj@live.com>
2021-04-11 04:47:40 +0530

    Add more tests

---
+++ test/IntegrationTests/NuGetClientTests/GetPackage_should.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using ApprovalTests.Reporters.TestFrameworks;
+
+using Jeevan.NuGetClient.IntegrationTests.Common;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Jeevan.NuGetClient.IntegrationTests.NuGetClientTests
+{
+    public sealed class GetPackage_should : NuGetSourcesBaseTests
+    {
+        public GetPackage_should(NuGetSourcesFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Fact]
+        [UseReporter(typeof(XUnit2Reporter))]
+        public async Task Get_nuget_package_stream()
+        {
+            Stream? packageStream = await Fixture.Client.GetPackageAsync("Collections.NET", "1.7.0");
+            packageStream.ShouldNotBeNull();
+
+            await using var ms = new MemoryStream();
+            packageStream.Seek(0, SeekOrigin.Begin);
+            await packageStream.CopyToAsync(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+
+            Approvals.VerifyBinaryFile(ms.ToArray(), ".nupkg.zip");
+        }
+    }
+}
---
+++ test/IntegrationTests/NuGetVersionTests/Ctor_should.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Semver;
+
+using Shouldly;
+
+using Xunit;
+
+namespace Jeevan.NuGetClient.IntegrationTests.NuGetVersionTests
+{
+    public sealed class Ctor_should
+    {
+        [Theory]
+        [InlineData("1.2.3")]
+        [InlineData("1.2.3+456")]
+        [InlineData("1.2.3-beta.4")]
+        [InlineData("1.2.3-beta.4+567")]
+        [InlineData("1.2.3.4")]
+        public void Initialize_from_version_strings(string versionStr)
+        {
+            var versionFromCtor = new NuGetVersion(versionStr);
+            NuGetVersion versionFromImplicitCast = versionStr;
+
+            versionFromCtor.ToString().ShouldBe(versionStr);
+            versionFromImplicitCast.ToString().ShouldBe(versionStr);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData("Jeevan")]
+        [InlineData("1a.2b.3c")]
+        public void Throw_for_invalid_version_strings(string versionStr)
+        {
+            Should.Throw<ArgumentException>(() => new NuGetVersion(versionStr));
+            Should.Throw<ArgumentException>(() =>
+            {
+#pragma warning disable SA1312 // Variable names should begin with lower-case letter
+                NuGetVersion _ = versionStr;
+#pragma warning restore SA1312 // Variable names should begin with lower-case letter
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSemanticVersions))]
+        public void Initialize_from_semver(SemVersion semver)
+        {
+            NuGetVersion version = semver;
+            version.ToString().ShouldBe(semver.ToString());
+        }
+
+        private static IEnumerable<object[]> GetSemanticVersions()
+        {
+            yield return new object[] { new SemVersion(1, 2, 3) };
+            yield return new object[] { new SemVersion(1, 2, 3, "beta.4") };
+            yield return new object[] { new SemVersion(1, 2, 3, "beta.4", "567") };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetVersions))]
+        public void Initialize_from_version(Version fullVer)
+        {
+            NuGetVersion version = fullVer;
+            version.ToString().ShouldBe(fullVer.ToString());
+        }
+
+        private static IEnumerable<object[]> GetVersions()
+        {
+            yield return new object[] { new Version(1, 2) };
+            yield return new object[] { new Version(1, 2, 3) };
+            yield return new object[] { new Version(1, 2, 3, 4) };
+        }
+    }
+}
